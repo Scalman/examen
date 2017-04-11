8b810a163ae6acfb89644a9e54ed39e68354a008
Scalman <waleedhassan@live.se>
2017-04-11 10:15:04 +0200

    mer

--- UDPStreamListener.py
+++ UDPStreamListener.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-
+import socket
+import struct
 
 class UDPStreamListener:
 
@@ -12,3 +13,17 @@ class UDPStreamListener:
 
     def perticipateToStream(self):
         print("listen to multicast stream")
+        UDP_IP = '233.12.166.100'
+        UDP_PORT = 1234
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind((UDP_IP, UDP_PORT))
+
+        while True:
+            data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+            #print "received message:", data
+            data = data.split()
+            data = map(lambda x: int(x,16), data)
+            data = struct.pack("%dB" % len(data), *data)
+            #print ' '.join('%02X' % ord(x) for x in data)
+            #print 'Checksum: '
+            # #, checksum(data)
